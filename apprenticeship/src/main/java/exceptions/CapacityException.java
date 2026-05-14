dc2d84a66748caae1fe90153da730dd2c59731ce
fotue2007 <joycemogueo@gmail.com>
2026-05-14 17:45:50 +0200

    Realisation des Exceptions

---
+++ apprenticeship/src/main/java/exceptions/CapacityException.java
@@ -0,0 +1,8 @@
+package main.java.exceptions;
+
+public class CapacityException extends Exception{
+
+	public CapacityException(String message) {
+		super(message);
+	}
+}
---
+++ apprenticeship/src/main/java/exceptions/PaymentException.java
@@ -0,0 +1,8 @@
+package main.java.exceptions;
+
+public class PaymentException extends Exception{
+
+	public PaymentException(String message) {
+		super(message);
+	}
+}
