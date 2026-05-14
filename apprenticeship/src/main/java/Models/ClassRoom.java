df6ff951cfc39c10c8ccc30af216eae78ac24be8
fotue2007 <joycemogueo@gmail.com>
2026-05-14 16:35:09 +0200

    Commit

--- apprenticeship/src/main/java/Models/ClassRoom.java
+++ apprenticeship/src/main/java/Models/ClassRoom.java
@@ -1,5 +1,24 @@
-package apprenticeship;
+package main.java.Models;
 
 public class ClassRoom {
 
+    private int id;
+
+    private String name;
+
+    private int capacity;
+
+    public ClassRoom() {
+    }
+
+    public ClassRoom(int id,
+                     String name,
+                     int capacity) {
+
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    // GETTERS & SETTERS
 }
--- apprenticeship/src/main/java/Models/TimeSlot.java
+++ apprenticeship/src/main/java/Models/TimeSlot.java
@@ -1,5 +1,52 @@
-package apprenticeship;
+package main.java.Models;
 
-public class TimeSlots {
+import enums.TimeSlotState;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeSlot {
+
+    private int id;
+
+    private String subject;
+
+    private LocalDateTime schedule;
+
+    private ClassRoom classroom;
+
+    private int maxCapacity;
+
+    private TimeSlotState state;
+
+    private List<Registration> registrations;
+
+    public TimeSlot() {
+
+        registrations = new ArrayList<>();
+    }
+
+    public TimeSlot(int id,
+                    String subject,
+                    LocalDateTime schedule,
+                    Classroom classroom,
+                    int maxCapacity) {
+
+        this.id = id;
+        this.subject = subject;
+        this.schedule = schedule;
+        this.classroom = classroom;
+        this.maxCapacity = maxCapacity;
+
+        this.state = TimeSlotState.AVAILABLE;
+
+        registrations = new ArrayList<>();
+    }
+
+    public boolean isFull() {
+        return registrations.size() >= maxCapacity;
+    }
+
+    // GETTERS & SETTERS
 }
