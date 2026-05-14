179d1400a7a984e68875dd36a26b820a8b6e1206
emma12-M <mekodjoe@3il.fr>
2026-05-14 16:57:17 +0200

    strategy

--- apprenticeship/src/main/java/strategies/SplitPayment.java
+++ apprenticeship/src/main/java/strategies/SplitPayment.java
@@ -1,7 +1,17 @@
-package strategyPattern;
+package main.java.strategies;
 
-import apprenticeship.PaymentStrategy;
+import main.java.interfaces.PaymentStrategy;
 
-public class SplitPayment implements PaymentStrategy{
+public class SplitPayment implements PaymentStrategy {
 
+	@Override
+    public double calculatePayment(double totalAmount,
+                                   int installmentCount) {
+
+        if(installmentCount <= 0) {
+            return totalAmount;
+        }
+
+        return totalAmount / installmentCount;
+    }
 }
